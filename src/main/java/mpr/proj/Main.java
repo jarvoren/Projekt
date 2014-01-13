87bc9025cce27defc461fae32b2c8ec8461cabfa
Jakub Dąbek <s10795@pjwstk.edu.pl>
2014-01-13 01:28:35 +0100

    Próbujemy :D

--- src/main/java/mpr/proj/Main.java
+++ src/main/java/mpr/proj/Main.java
@@ -1,7 +1,76 @@
 package mpr.proj;
+import java.lang.*;
+import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+import mpr.proj.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ahoj!");
+      int w=0;
+    	do{
+    		
+    	try {
+			Runtime.getRuntime().exec("cls");
+		} catch (IOException e) {
+			
+		}	
+    	System.out.println("Menu");
+        System.out.println("1) Dodaj wpis");
+        System.out.println("2) Wypisz tabele");
+        System.out.println("3) Updatuj wpis");
+        System.out.println("4) Skasuj wpis");
+        System.out.println("5) Wyszukaj potomstwo konia");
+        System.out.println("6) Generuj rodowód w pdf");
+        System.out.println("7) Wyjscie");
+        
+        
+        
+        switch(EasyIn.getInt())
+        {
+        case 1: {
+        	insideMenus.dodawanieWpisu();
+        	
+        	break;
+        }
+        case 2: {
+        	
+        	insideMenus.wypisywanieDanych();
+        	break;
+        }
+        case 3:{
+        	insideMenus.updateDanych();
+        	break;
+        }
+        case 4:{
+        	insideMenus.kasowanieDanych();
+        	break;
+        }
+        case 5:{
+        	insideMenus.wyszukiwaniePotomstwa();
+        	break;
+        }
+        case 6:{
+        	insideMenus.generowanieRodowodu();
+        	break;
+        }
+        case 7:{
+        	w=1;
+        	break;
+        }
+        
+        
+        default :{
+        	 System.out.println("Nieprawidłowy wybór try again");
+        	 EasyIn.getChar();
+        	
+        }
+        }
+        
+        
+        
+        
+    	}while(w==0);
     }
+
 }
---
+++ src/main/java/mpr/proj/insideMenus.java
@@ -0,0 +1,24 @@
+package mpr.proj;
+
+public abstract class insideMenus {
+	
+	public static void dodawanieWpisu(){
+		
+	}
+	public static void wypisywanieDanych(){
+		
+	}
+	public static void updateDanych(){
+		
+	}
+	public static void kasowanieDanych(){
+		
+	}
+	public static void wyszukiwaniePotomstwa(){
+		
+	}
+	public static void generowanieRodowodu(){
+		
+	}
+
+}
